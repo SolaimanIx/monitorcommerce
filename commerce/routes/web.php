64b576371054b87ed5d69182c0e47c4330f8d297
SolaimanIx <hasheema.md@gmail.com>
2025-03-08 01:50:47 +0200

    application layout added, HomeController and Index view and route

--- commerce/routes/web.php
+++ commerce/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');

221ba8247840d58f5ec10e8398af37e40df57dbf
SolaimanIx <hasheema.md@gmail.com>
2025-03-10 17:18:37 +0200

    added feature to Admin panel to edit and update products, fixed a bug where update product didn't work

--- commerce/routes/web.php
+++ commerce/routes/web.php
@@ -19,6 +19,8 @@ Route::middleware(['auth'])->group(function(){
     Route::get('/account-dashboard',[UserController::class,'index'])->name('user.account.dashboard');
 });
 Route::middleware(['auth', AuthAdmin::class])->group(function(){
+    // Routes for the brands section of the admin page
+    // This section includes routes for viewing, adding, editing, updating, and deleting brands.
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
     Route::get('/admin/brands', [AdminController::class, 'brands'])->name('admin.brands');
     Route::get('/admin/brand/add', [AdminController::class, 'add_brand'])->name('admin.brand.add');
@@ -26,11 +28,22 @@ Route::middleware(['auth', AuthAdmin::class])->group(function(){
     Route::get('/admin/brand/edit/{id}', [AdminController::class, 'brand_edit'])->name('admin.brand.edit');
     Route::put('/admin/brand/update', [AdminController::class, 'brand_update'])->name('admin.brand.update');
     Route::delete('/admin/brand/{id}/delete', [AdminController::class, 'brand_delete'])->name('admin.brand.delete');
-
+    // Routes for the categories section of the admin page
+    // This section includes routes for viewing, adding, editing, updating, and deleting categories.
     Route::get('/admin/categories', [AdminController::class, 'categories'])->name('admin.categories');
     Route::get('/admin/category/add', [AdminController::class, 'category_add'])->name('admin.category.add');
     Route::post('/admin/category/store', [AdminController::class, 'category_store'])->name('admin.category.store');
     Route::get('/admin/category/{id}/edit', [AdminController::class, 'category_edit'])->name('admin.category.edit');
     Route::put('/admin/category/update', [AdminController::class, 'category_update'])->name('admin.category.update');
     Route::delete('/admin/category/{id}/delete', [AdminController::class, 'category_delete'])->name('admin.category.delete');
+
+    Route::get('/admin/products', [AdminController::class, 'products'])->name('admin.products');
+    Route::get('/admin/product/add', [AdminController::class, 'product_add'])->name('admin.product.add');
+    Route::post('/admin/product/store', [AdminController::class, 'product_store'])->name('admin.product.store');
+    Route::get('/admin/product/{id}/edit', [AdminController::class, 'product_edit'])->name('admin.product.edit');
+    Route::put('/admin/product/update', [AdminController::class, 'product_update'])->name('admin.product.update');
+    Route::delete('/admin/product/{id}/delete', [AdminController::class, 'product_delete'])->name('admin.product.delete');
+
 });
+
+

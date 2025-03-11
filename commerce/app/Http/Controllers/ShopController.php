08c5be0aa5392ad02aff71098d84246348299a81
SolaimanIx <hasheema.md@gmail.com>
2025-03-11 23:10:11 +0200

    added sorting by brand "bugged and not working"

--- commerce/app/Http/Controllers/ShopController.php
+++ commerce/app/Http/Controllers/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
@@ -13,6 +14,7 @@ class ShopController extends Controller
     $o_column = "";
     $o_order = "";
     $order = $request->query('order') ? $request->query('order') : -1;
+    $f_brands = $request->query('brands');
     switch($order) {
         case 1:
             $o_column = 'created_at';
@@ -34,10 +36,15 @@ class ShopController extends Controller
             $o_column = 'id';
             $o_order = 'DESC';
     }
+    $brands = Brand::orderBy('name', 'ASC')->get();
+    $products = Product::where(function($query) use($f_brands) {
+        $query->whereIn('brand_id', explode(',', $f_brands))
+              ->orWhereRaw("'" . $f_brands . "'=''");
+    })
+    ->orderBy($o_column, $o_order)
+    ->paginate($size);
     
-    $products = Product::orderBy($o_column, $o_order)->paginate($size);
-    
-    return view('shop', compact('products', 'size', 'order'));
+    return view('shop', compact('products', 'size', 'order', 'brands', 'f_brands'));    
     }
 
     public function product_details($product_slug)
